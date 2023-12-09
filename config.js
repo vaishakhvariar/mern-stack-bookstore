fd0822fd3a35e93de75d50da915a39e1a2c310e9
Vaishakh Variar <vaishakhvariar@gmail.com>
2023-12-09 20:53:47 +0530

    feat: Set up Express server and MongoDB database. Implemented CRUD on database

---
+++ config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;   
+
+export const mongoDBURL = 'mongodb+srv://vaishakhvariar:7d9W6NljEVVrSdqj@cluster0.edkn23w.mongodb.net/mern-stack-bookstore?retryWrites=true&w=majority';
+
